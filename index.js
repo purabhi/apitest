d5a64d6d748335be3a581439dc0f108cc00c73ea
purnima <98867198+purabhi@users.noreply.github.com>
2022-08-20 11:09:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -22,11 +22,12 @@ const persondetails = new mongoose.Schema({
 })
 const details = mongoose.model('details', persondetails)
 app.get('/',function (req, res) {res.send('Hello World')})
-app.get('/login/:name/:pass', async function (req, res) {
-    try 
-    {
-        let name = req.params.name
-        let password = req.params.pass
+app.get('/personRegister/users', async function (req, res) {
+    try {
+
+       
+        let name = req.query.name;
+        let password = req.query.pass;
 
         if (name === undefined &&  password === undefined) {
             res.status(401).send('pls fill the fields')
@@ -35,30 +36,69 @@ app.get('/login/:name/:pass', async function (req, res) {
             res.status(401).send('pls fill the fields')
         }
         else {
-            if (password == '' || password == undefined) {
+            if (name == '' || name == undefined) {
+                res.status(401).send('pls fill the name field')
+            }
+            else if (password == '' || password == undefined) {
                 res.status(401).send('pls fill the Password field')
             }
-            else if (name == '' || name == undefined) {
-                res.status(401).send('pls fill the Name field')
-            }
-           
-            else 
-                {
+            else {
+                const user = await details.findOne({ pass: password })
+                if (user) {
+                    res.status(401).send('User already exists')
+                }
+                else {
                     const user = new details({
                         name: name,
+                     
                         pass: password
                     })
                     await user.save()
-                    res.status(200).send({name})
-                
+                    res.status(200).send('Registerd successfully')
+                }
             }
         }
-    
     }
-    
     catch (err) {
         console.log(err)
         res.status(500).send('Server error')
+    }
+})
+
+
+app.get('/personLogin/users', async function (req, res) {
+
+    try {
+        let name = req.query.name
+        let password = req.query.pass
+       
+        
+        if (name == "" && password == "") {
+            res.status(401).send("Please Fill All Fields Properly")
+        }
+        else if (name === undefined && password === undefined) {
+            res.status(401).send("Please Fill All Fields Properly")
+        }
+        else {
+            if (name == undefined || name == "") {
+                res.send("Please Fill the Name Field ")
+            }
+            else if (password == undefined || password == "") {
+                res.status(401).send("Please Fill the password ")
+            }
+            else {
+
+                    const user1 = await details.findOne({ pass: req.query.pass })
+                    res.status(201).send({user1})
+                       
+                    }
+                }
+            }
+        
+    
+    catch (err) {
+        console.log(err);
+        res.status(500).send('Server Error')
     }
 })
 
